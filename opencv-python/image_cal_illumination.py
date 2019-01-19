79bde7388c6f83b7eff250a80652c07e0ff78677
yangshuai <yangshuai7@outlook.com>
2019-01-19 19:10:31 +0800

    r720:研究多线程、进程池等；opencv直方图

--- opencv-python/image_cal_illumination.py
+++ opencv-python/image_cal_illumination.py
@@ -4,90 +4,66 @@
 '''
 @author: YangShuai
 @category: opencv-python
-@application: 判断图像亮度，是否过曝或过暗
-@creation time: 20190114
+@application: 判断图像亮度
+@creation time: 20190119
 @modify time:
 '''
-# 参考：https://blog.csdn.net/u014453898/article/details/80745987
-# 先把图片转换为灰度图,然后根据灰度值的分布来判断
-# 要判断图片的亮暗,只需要统计偏暗的像素个数,再除以图片像素的总个数,得到百分比p即可,至于p大于多少即判断为暗,则可以由你自己设置。
+
+# 先把图片转换为灰度图,然后根据灰度值分布来判断
+# 要判断图片的亮暗,只需要统计偏暗的像素个数,再除以图片像素的总个数,得到百分比p即可,至于p大于多少即判断为暗,则可以由你自己设置
+
 from __future__ import print_function
-import cv2
-import matplotlib.pyplot as plt
-import os
-import sys
+import cv2, os, shutil, sys
 
-# 运行命令: python3 cal.py 图片集的路径名
-def func(img, pic_path, pic):
-    # 把图片转换为灰度图
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # 获取灰度图矩阵的行数和列数
-    r, c = gray_img.shape[:2]
-    dark_sum = 0  # 偏暗的像素 初始化为0个
-    # dark_prop = 0  # 偏暗像素所占比例初始化为0
-    piexs_sum = r * c  # 整个灰度图的像素个数为r*c
+def judgeIllumination(image_path, dark_dir, bright_dir, pixel_threshold=128, image_threshold=0.7, ):
+    """
+    :param image_path: 原始图像路径
+    :param pixel_threshold: 像素阈值超参数，像素值低于此值则认为是暗像素
+    :param image_threshold: 图像阈值超参数，dark_prop低于此值则认为是暗图像
+    :param dark_dir: 保存暗图像的目录
+    :param bright_dir: 保存亮图像的目录
+    :return:
+    """
+    if not os.path.exists(dark_dir):
+        os.mkdir(dark_dir)
+        print('create dir: ', dark_dir)
+    if not os.path.exists(bright_dir):
+        os.mkdir(bright_dir)
+        print('create dir: ', bright_dir)
 
-    # 遍历灰度图的所有像素
-    for row in gray_img:
-        for colum in row:
-            if colum < 128:  # 人为设置的超参数,表示0~39的灰度值为暗
-                dark_sum += 1
-    dark_prop = dark_sum / piexs_sum
-    print("dark_sum:", dark_sum)
-    print("piexs_sum:", piexs_sum)
-    print("dark_prop=dark_sum/piexs_sum:", dark_prop)
-    if dark_prop >= 0.6:  # 人为设置的超参数:表示若偏暗像素所占比例超过0.78,则这张图被认为整体环境黑暗的图片
-        print(pic_path + " is dark!")
-        cv2.imwrite("../DarkPicDir/" + pic, img)  # 把被认为黑暗的图片保存
+    image = cv2.imread(image_path)
+    #转为灰度图
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #获取灰度图矩阵的行数和列数
+    rows, cols = image_gray.shape[:2]
+    total_pixels = rows * cols #图像总像素数量
+    dark_pixels = 0 #偏暗的像素数量，初始化为0个
+    dark_prop = 0 #偏暗的像素占总像素比例，初始化为0
+
+    #遍历灰度图的所有像素
+    for row in image_gray:
+        for col in row:
+            if col < pixel_threshold:
+                dark_pixels += 1
+    dark_prop = dark_pixels / total_pixels
+    print('total_pixels:{}, dark_pixels:{}, dark_prop:{}'.format(total_pixels, dark_pixels,dark_prop))
+
+    if dark_prop > image_threshold:
+        print(image_path + ' is dark.')
+        shutil.copy(image_path, dark_dir)
     else:
-        print(pic_path + " is bright!")
-
-
-# hist(pic_path)  #若要查看图片的灰度值分布情况,可以这个注释解除
-
-# 用于显示图片的灰度直方图
-def hist(pic_path):
-    img = cv2.imread(pic_path, 0)
-    hist = cv2.calcHist([img], [0], None, [256], [0, 256])
-    plt.subplot(121)
-    plt.imshow(img, 'gray')
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Original")
-    plt.subplot(122)
-    plt.hist(img.ravel(), 256, [0, 256])
-    plt.show()
-
-
-# 读取给定目录的所有图片
-def readAllPictures(pics_path):
-    if not os.path.exists(pics_path):
-        print("路径错误，路径不存在！")
-        return
-    allPics = []
-    pics = os.listdir(pics_path)
-    for pic in pics:
-        pic_path = os.path.join(pics_path, pic)
-        if os.path.isfile(pic_path):
-            allPics.append(pic_path)
-            img = cv2.imread(pic_path)
-            func(img, pic_path, pic)
-    return allPics
-
-
-# 创建用于存放黑暗图片的目录
-def createDarkDir():
-    DarkDirPath = "../DarkPicDir"
-    isExists = os.path.exists(DarkDirPath)
-    if not isExists:
-        os.makedirs(DarkDirPath)
-        print("dark pics dir is created successfully!")
-        return True
-    else:
-        return False
-
+        print(image_path + ' is bright.')
+        shutil.copy(image_path, bright_dir)
 
 if __name__ == '__main__':
-    pics_path = sys.argv[1]  # 获取所给图片目录
-    createDarkDir()
-    allPics = readAllPictures(pics_path)
+    image_dir = '/home/ys/Github/opencv_prac/illumination/'
+    dark_dir = '/home/ys/Github/opencv_prac/dark/'
+    bright_dir = '/home/ys/Github/opencv_prac/bright/'
+    image_list = os.listdir(image_dir)
+    for image in image_list:
+        image_path = image_dir + image
+        pixel_threshold = sys.argv[1]
+        image_threshold = sys.argv[2]
+        pixel_threshold = int(pixel_threshold)
+        image_threshold = float(image_threshold)
+        judgeIllumination(image_path, dark_dir, bright_dir, pixel_threshold, image_threshold)
